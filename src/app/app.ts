5aa5cbf478ba4a689be82d1fd5f4454a52f7277f
Stanislav Stanchev <stanislav.stanchev@rewe-group.com>
2025-11-12 17:44:17 +0200

    Broken now, will fix later

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,78 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, Input} from '@angular/core';
+import {MatFormField} from '@angular/material/input';
+import {MatInput} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatListOption, MatSelectionList} from '@angular/material/list';
+
+interface InputItem {
+  value: string,
+  selected: boolean
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [
+    MatFormField,
+    MatInput,
+    MatButton,
+    FormsModule,
+    MatSelectionList,
+    MatListOption
+  ]
 })
 export class App {
-  protected readonly title = signal('material-todo');
+
+  inputItems: InputItem[] = []
+  public public: InputItem[] = (() => this.publicData())();
+
+  @Input()
+  inputData: string = ""
+
+  protected onAdd() {
+    this.inputItems.push({value: this.inputData, selected: false});
+    this.inputData = "";
+  }
+
+  protected onDelete() {
+    let newArray: InputItem[] = [];
+    for (let item of this.inputItems) {
+      if (!item.selected) {
+        newArray.push(item);
+      }
+    }
+    this.inputItems = newArray;
+  }
+
+  protected onSelectChanged(item: InputItem) {
+    item.selected = !item.selected;
+  }
+
+  protected filterItems() {
+
+  }
+
+  protected publicData() {
+    if (this.inputData.length === 0) {
+      return this.inputItems;
+    }
+    let filtered: InputItem[] = [];
+    for (let inputItem of this.inputItems) {
+      if (inputItem.value.includes(this.inputData)) {
+        filtered.push(inputItem)
+      }
+    }
+
+    return filtered;
+  }
+
+  protected hasSelectedItems() {
+    for (let inputItem of this.inputItems) {
+      if (inputItem.selected) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
